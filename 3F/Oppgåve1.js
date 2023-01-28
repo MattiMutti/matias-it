9fe6640720ecf40afef7b821ae6e9ab1f97b3554
Matias Mittigård <matiasmittigard@gmail.com>
2023-01-28 17:29:02 +0100

    Oppgave 1

---
+++ 3F/Oppgåve1.js
@@ -0,0 +1,23 @@
+let informasjonsteknologi1 = {
+    spraak: ["HTML", "CSS"],
+    likerFaget: false,
+    timetall: 5,
+    klasserom: "C14",
+    antallElever: 24
+  };
+
+informasjonsteknologi1.spraak.push("Javascript");
+
+informasjonsteknologi1.antallElever = "7";
+
+delete informasjonsteknologi1.timetall;
+
+for (let egenskap in informasjonsteknologi1.spraak) {
+    console.log(informasjonsteknologi1.spraak);
+  }
+
+for (let egenskap in informasjonsteknologi1) {
+    console.log([informasjonsteknologi1]);
+  }
+
+ 
