87e275dc2292a597ef7a69409eed9c24effd754a
Matias Mittigård <matiasmittigard@gmail.com>
2023-02-06 12:47:25 +0100

    Oppgave 9 og 10

---
+++ 4A/oppgave9-14/oppgave9-14.js
@@ -0,0 +1,11 @@
+let overskrift = document.querySelector("h1"); 
+let avsnitt1 = document.querySelector("p1"); 
+let avsnitt2 = document.querySelector("p2"); 
+
+overskrift.innerHTML = "<h1> Dette avsnittet erstatter alt innhold </h1>";
+avsnitt1.innerHTML = "<p1> Dette avsnittet erstatter alt innhold </p1>";
+avsnitt2.innerHTML = "<p2> Dette avsnittet erstatter alt innhold </p2>";
+
+console.log(overskrift)
+console.log(avsnitt1)
+console.log(avsnitt2)
