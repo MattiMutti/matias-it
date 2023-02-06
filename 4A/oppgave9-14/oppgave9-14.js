664f9f0b60b58a416c87e9c66c4b4bc2f4072bed
Matias Mittigård <matiasmittigard@gmail.com>
2023-02-06 13:05:58 +0100

    oppgave 11

--- 4A/oppgave9-14/oppgave9-14.js
+++ 4A/oppgave9-14/oppgave9-14.js
@@ -1,3 +1,7 @@
+/*
+Oppgave 9 og 10, lager dokument og deretter bruker innerhtml til å erstatte innholdet
+*/
+
 let overskrift = document.querySelector("h1"); 
 let avsnitt1 = document.querySelector("p1"); 
 let avsnitt2 = document.querySelector("p2"); 
@@ -9,3 +13,47 @@ avsnitt2.innerHTML = "<p2> Dette avsnittet erstatter alt innhold </p2>";
 console.log(overskrift)
 console.log(avsnitt1)
 console.log(avsnitt2)
+
+
+let bodyEl = document.querySelector("body");
+
+tabell += "<table>";
+tabell += "<thead>";
+tabell += "<tr>";
+tabell += "<th>Overskrift 1</th>";
+tabell += "<th>Overskrift 2</th>";
+tabell += "</tr>";
+tabell += "</thead>";
+tabell += "<tbody>";
+tabell += "<tr>";
+tabell += "<td>Innhold 1</td>";
+tabell += "<td>Innhold 2</td>";
+tabell += "</tr>";
+tabell += "</tbody>";
+tabell += "</table>";
+
+bodyEl.innerHTML += tabell;
+
+let liste = document.querySelector("body")
+
+let tabell = "";
+
+tabell += "<table>";
+tabell += "<thead>";
+tabell += "<tr>";
+tabell += "<th>Overskrift 1</th>";
+tabell += "<th>Overskrift 2</th>";
+tabell += "</tr>";
+tabell += "</thead>";
+tabell += "<tbody>";
+tabell += "<tr>";
+tabell += "<td>Innhold 1</td>";
+tabell += "<td>Innhold 2</td>";
+tabell += "</tr>";
+tabell += "</tbody>";
+tabell += "</table>";
+
+body.innerHTML += "<li id='liste'>liste</li>";
+body.innerHTML += "<ul id='liste'>gulerotkake</ul>";
+body.innerHTML += "<ul id='liste'>fleinsopp</ul>";
+
