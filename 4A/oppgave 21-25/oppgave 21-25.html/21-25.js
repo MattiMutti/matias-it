d185c25146c4390b16005128f44f6fb4cf412ac9
Matias Mittigård <matiasmittigard@gmail.com>
2023-02-16 10:39:23 +0100

    oppgave 21 og 22

---
+++ 4A/oppgave 21-25/oppgave 21-25.html/21-25.js
@@ -0,0 +1,16 @@
+let bodyEl = document.querySelector("body");
+
+let liste = "";
+
+let liste1El = document.createElement("ul");
+let liste2El = document.createElement("li");
+let liste3El = document.createElement("li");
+let liste4El = document.createElement("ul");
+
+listeEl.id = "liste"
+
+listeEl.setAttribute("class", "red");
+
+
+bodyEl.appendChild(listeEl);
+
