fc22e939f8e15db0390dbbba33a7c92f9eb2be6f
Matias Mittigård <matiasmittigard@gmail.com>
2023-03-27 13:26:31 +0200

    La inn betre oversikt på hovedsida

---
+++ 4B/Oppgave 1.js
@@ -0,0 +1,10 @@
+/* 
+Oppgave 1 a
+*/
+
+const boks1El = document.querySelector("#boks1");
+console.log(boks1EL);
+boks1EL.addEventListener("click", duKlikketMeg);
+function duKlikketMeg() {
+    boks1EL.innerHTML = "du klikket på meg";
+}
